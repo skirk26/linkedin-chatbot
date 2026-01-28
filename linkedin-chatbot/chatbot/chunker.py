a5439d958c25cd33c77a41affd908aa4ede4fdb3
Sam Kirkhorn <samkirkhorn@gmail.com>
2026-01-27 20:31:12 -0500

    Delete linkedin-chatbot directory

--- linkedin-chatbot/chatbot/chunker.py
+++
@@ -1,7 +0,0 @@
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-
-def chunker(profile_string):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 100)
-    chunks = text_splitter.split_text(profile_string)
-    return chunks
-
